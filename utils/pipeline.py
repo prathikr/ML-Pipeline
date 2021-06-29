a05ae4413ffb904e409088db91539b08ad6bcfdf
Prathik Rao <prathikr@Prathiks-M1-MacBook-Pro.local>
2021-06-28 19:16:40 -0700

    cleaned example dataset

--- utils/pipeline.py
+++ utils/pipeline.py
@@ -3,12 +3,15 @@ import json
 class Pipeline():
 
     def __init__(self, config_filename):
+        print("--- __init__ ---")
         self.config_filename = config_filename
         self.validate_config()
 
     def validate_config(self):
+        print("--- validate config ---")
         with open(self.config_filename) as f:
             data = json.load(f)
+        print("Config:", data)
         return True
 
     def preprocess(self):
@@ -22,3 +25,6 @@ class Pipeline():
     def postprocess(self):
         print("--- postprocessing ---")
         return True
+
+if __name__ == '__main__':
+    p = Pipeline('/Users/prathikr/Desktop/CAIS/ML-Pipeline/example_config.json')
