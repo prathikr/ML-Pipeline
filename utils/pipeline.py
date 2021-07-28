4ddfd62ecdf791d7284181c1fc5eb065dc16fcab
Prathik Rao <prathikr@Prathiks-M1-MacBook-Pro.local>
2021-07-28 15:25:30 -0700

    permutation importance

--- utils/pipeline.py
+++ utils/pipeline.py
@@ -1,11 +1,14 @@
 import json
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import cross_validate
+#from sklearn.model_selection import cross_validate
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import get_scorer
 from utils.preprocess import *
 from utils.model import *
 from utils.postprocess import *
 from math import sqrt
+from sklearn.base import clone
 
 class Pipeline():
 
@@ -44,11 +47,33 @@ class Pipeline():
 
             self.X = self.X[optimal_features]
 
-        self.scores = cross_validate(clone(model), self.X, self.y, cv=5, scoring=[model_args['optimization_metric']], return_estimator=True)
+        #self.scores = cross_validate(clone(model), self.X, self.y, cv=5, scoring=[model_args['optimization_metric']], return_estimator=True)
+        # Create StratifiedKFold object.
+        skf = StratifiedKFold(n_splits=5, shuffle=True)
+        scorer = get_scorer(model_args['optimization_metric'])
+        cross_val_scores = []
+        self.KFold_data = {}
+        self.estimators = []
+        fold = 0
+        for train_index, test_index in skf.split(self.X, self.y):
+            x_train_fold, x_test_fold = self.X.iloc[train_index], self.X.iloc[test_index]
+            y_train_fold, y_test_fold = self.y.iloc[train_index], self.y.iloc[test_index]
+            self.KFold_data[fold] = {'x_train_fold': x_train_fold, 'x_test_fold': x_test_fold, 'y_train_fold': y_train_fold, 'y_test_fold': y_test_fold}
+            model = get_model(self.model_name, model_args['importlib'], model_args['params'])
+            self.estimators.append(model.fit(x_train_fold, y_train_fold))
+            y_pred = self.estimators[fold].predict(x_test_fold)
+            cross_val_scores.append(scorer._score_func(y_test_fold, y_pred))
+            fold += 1
+        
         confidence_level = 2.776 # corresponding z-score value for 95% confidence and 4 degrees of freedom (since we do 5 fold cv)
-        print(model_args['optimization_metric'], 'performance across 5 folds with 95% confidence:' ,round(self.scores['test_' + model_args['optimization_metric']].mean(), 4), '+/-', round(confidence_level * self.scores['test_' + model_args['optimization_metric']].std() / sqrt(len(self.scores['test_' + model_args['optimization_metric']])), 4))
+        print(model_args['optimization_metric'], 'performance across 5 folds with 95% confidence:', round(np.array(cross_val_scores).mean(), 4), '+/-', round(confidence_level * np.array(cross_val_scores).std() / sqrt(len(cross_val_scores)), 4))
 
     def postprocess(self):
         postprocess_args = self.config['postprocess']
+        model_args = self.config['model']
 
-        self.feature_importance = feature_importance(self.scores['estimator'], self.model_name, postprocess_args['feature_importance_strategy'], self.X.columns)
+        if postprocess_args['feature_importance_strategy'] == 'built_in':
+            feature_importance_built_in(self.estimators, self.model_name, self.X.columns)
+        elif postprocess_args['feature_importance_strategy'] == 'permutation_importance':
+            model = get_model(self.model_name, model_args['importlib'], model_args['params'])
+            feature_importance_permutation_importance(model, self.KFold_data)
--- utils/postprocess.py
+++ utils/postprocess.py
@@ -2,10 +2,11 @@ import pandas as pd
 from math import sqrt
 import numpy as np
 from sklearn.inspection import permutation_importance
+from sklearn.base import clone
 
 # collect feature importance values across training folds
-def feature_importance(models, model_name, strategy, features):
-    print('--- feature_importance ---')
+def feature_importance_built_in(models, model_name, features):
+    print('--- feature_importance_built_in ---')
 
     d = {'feat': features} # create feature importance dataframe
     col_headers = []
@@ -13,14 +14,50 @@ def feature_importance(models, model_name, strategy, features):
     idx = 1
     for model in models:
         col_header = 'feat_imp_fold' + str(idx)
-        if strategy == 'built_in':
-            if model_name == 'LogisticRegression':
-                d[col_header] = model.coef_[0]
-            else:
-                d[col_header] = model.feature_importances_
+        if model_name == 'LogisticRegression':
+            d[col_header] = model.coef_[0]
+        else:
+            d[col_header] = model.feature_importances_
         col_headers.append(col_header)
         idx += 1
     
+    feat_imp = pd.DataFrame(data=d)
+    #print(feat_imp)
+    feat_imp['feat_imp_mean'] = feat_imp[col_headers].mean(axis=1)
+    feat_imp['feat_imp_std'] = feat_imp[col_headers].std(axis=1)
+    feat_imp['feat_imp_zeros'] = (feat_imp[col_headers] == 0).astype(int).sum(axis=1)
+
+    confidence_level = 2.776 # corresponding z-score value for 95% confidence and 4 degrees of freedom (since we do 5 fold cv)
+    feat_imp['feat_imp_95_ci'] = confidence_level * feat_imp['feat_imp_std'] / sqrt(len(col_headers))
+
+    feat_imp.sort_values(by='feat_imp_mean', ascending=False, inplace=True)
+    print(feat_imp[['feat', 'feat_imp_mean', 'feat_imp_95_ci']])
+    return feat_imp
+
+def feature_importance_permutation_importance(model, KFold_data):
+    print('--- feature_importance_permutation_importance ---')
+
+    d = {'feat': KFold_data[0]['x_train_fold'].columns} # create feature importance dataframe
+    col_headers = []
+
+    for fold in list(KFold_data.keys()):
+        col_header = 'feat_imp_fold' + str(fold)
+        col_headers.append(col_header)
+
+        fold_data = KFold_data[fold]
+
+        x_train_fold = fold_data['x_train_fold']
+        x_test_fold = fold_data['x_test_fold']
+        y_train_fold = fold_data['y_train_fold']
+        y_test_fold = fold_data['y_test_fold']
+
+        model_copy = clone(model)
+        model_copy.fit(x_train_fold, y_train_fold)
+
+        r = permutation_importance(model_copy, x_test_fold, y_test_fold, n_repeats=20)
+
+        d[col_header] = r.importances_mean
+
     feat_imp = pd.DataFrame(data=d)
 
     feat_imp['feat_imp_mean'] = feat_imp[col_headers].mean(axis=1)
